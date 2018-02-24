29e4bf4ea51bb010a3f39b503c2875544efa0210
ipcrm <mcadorette@gmail.com>
2018-02-24 09:17:04 -0500

    removing pritns

--- scripts/remove_vm.rb
+++ scripts/remove_vm.rb
@@ -5,32 +5,6 @@ vm_name    = ENV['vm_name']    || abort('VM Name not set [export vm_name=X]. Ful
 fog_config = ENV['fog_config'] || abort('Fog configuration not set [export fog_config=X]')
 datacenter = ENV['datacenter'] || abort('Datacenter not set [export datacenter=X]')
 
-require 'yaml'
-cnf = YAML::load(File.open('fog'))
-
-
-puts "VMName: #{vm_name}"
-puts "Fog Config: #{fog_config}"
-puts "Datacenter: #{datacenter}"
-
-
-
-
-begin
-  file = File.open("/tmp/some_file", "w")
-  file.write(vm_name)
-  file.write("\n")
-  file.write(fog_config)
-  file.write("\n")
-  file.write(datacenter)
-  file.write("\n")
-  file.write(cnf)
-  file.write("\n")
-rescue IOError => e
-  puts e.message
-ensure
-  file.close unless file.nil?
-end
 
 begin
   cfg = YAML.load_file(fog_config)
