fb74b4047a7171c04fa2595060cae22e30304de2
maju6406 <beersy@gmail.com>
2018-05-29 15:03:52 -0700

    Changing logic

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -189,12 +189,9 @@ FILE
   cd /tmp
   sleep 5
   sudo -u git /opt/gitea/gitea admin create-user --name=puppet --password=puppetlabs --email='puppet@localhost.local' --admin=true
-  for i in 1 2 3 4 5
-  do
-    if [ $? -ne 0 ]; then
-      echo "Attempting to create user again $i"
-      sudo -u git /opt/gitea/gitea admin create-user --name=puppet --password=puppetlabs --email='puppet@localhost.local' --admin=true
-    fi  
+  while [ $? -ne 0 ]; do
+    echo "Attempting to create user again"
+    sudo -u git /opt/gitea/gitea admin create-user --name=puppet --password=puppetlabs --email='puppet@localhost.local' --admin=true
   done
 
   if [ $? -ne 0 ]; then
@@ -204,12 +201,9 @@ FILE
 
   echo "{\"clone_addr\": \"${GIT_REMOTE}\", \"uid\": 1, \"repo_name\": \"control-repo\"}" > repo.data
   curl -H 'Content-Type: application/json' -X POST -d @repo.data http://puppet:puppetlabs@localhost:3000/api/v1/repos/migrate
-  for i in 1 2 3 4 5
-  do
-    if [ $? -ne 0 ]; then
-      echo "Attempting to migrate repos again $i"
-      curl -H 'Content-Type: application/json' -X POST -d @repo.data http://puppet:puppetlabs@localhost:3000/api/v1/repos/migrate
-    fi  
+  while [ $? -ne 0 ]; do
+    echo "Attempting to migrate repos"
+    curl -H 'Content-Type: application/json' -X POST -d @repo.data http://puppet:puppetlabs@localhost:3000/api/v1/repos/migrate
   done
 
   if [ $? -ne 0 ]; then
