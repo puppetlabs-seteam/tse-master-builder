b95ee9c027c8c2ebf077a01b640439509c6521f5
ipcrm <mcadorette@gmail.com>
2018-05-30 21:33:02 -0400

    try, try again

--- build.sh
+++ build.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 set -x
-#DEBUG
-GITHUB_USER_NAME=${CREDENTIALS%:*}
-GITHUB_USER_TOKEN=${CREDENTIALS#*:}
-
-env
-
 if [ -z $1 ]; then
   echo "MUST PASS BUILDER, virtualbox-ovf or vmware-vmx! Exiting..."
   exit 1
@@ -104,8 +98,8 @@ packer build \
   -parallel=false \
   -var "GIT_VERSION=$GIT_VERSION" \
   -var "GIT_REMOTE=$GIT_REMOTE" \
-  -var "GITHUB_USER_NAME=$GITHUB_USER_NAME" \
-  -var "GITHUB_USER_TOKEN=$GITHUB_USER_TOKEN" \
+  -var "GITHUB_USER_NAME=${CREDENTIALS%:*}" \
+  -var "GITHUB_USER_TOKEN=${CREDENTIALS#*:}" \
   -var "LIC_KEY=$LIC_KEY" \
   -var "BUILD_VER=$BUILD_VER" \
   -var "DOWNLOAD_VERSION=$DOWNLOAD_VERSION" \
